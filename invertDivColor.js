74f709391c1e3bb529e8eadb1961abd398c4046d
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-06-10 11:17:04 -0700

    solved challenge

---
+++ invertDivColor.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+ function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
+ //converts rgb to hex (source: http://jsfiddle.net/Mottie/xcqpF/1/light/)   
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}    
+    
+    $("div").each(function(){
+	var r = Math.round(Math.random() * 255);
+	var g = Math.round(Math.random() * 255);
+	var b = Math.round(Math.random() * 255);
+
+	$(this).css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+        var bgColor = $(this).css('background-color');
+        bgColor = rgb2hex(bgColor);
+        var color =  invertColor(bgColor);  
+        $(this).css("color", color);  
+    });
+    
+
+});
